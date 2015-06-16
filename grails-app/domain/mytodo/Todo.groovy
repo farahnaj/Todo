aa151c5212da59bb4e077da85d67b2a566273e4a
farahnaj <farahinamdar@gmail.com>
2015-06-16 18:36:18 +0530

    first commit

---
+++ grails-app/domain/mytodo/Todo.groovy
@@ -0,0 +1,14 @@
+package mytodo
+
+class Todo {
+    String title;
+    Date due_date;
+    boolean is_active = true;
+
+    static constraints = {
+    }
+
+    static mapping = {
+        table "Todo"
+    }
+}
